2abf97a376847126eef934a1a22bcc0f3ed6ed9b
Matt Yoon <yoon.mattyoon@gmail.com>
2021-07-26 03:30:18 -0400

    alert window with details working

--- main.js
+++ main.js
@@ -1,44 +1,24 @@
-const API_KEY = '4421ABB9-96C5-46C2-84A8-22EB00072AF7'
+// Variable to hold the secret key
+const API_KEY = '7A90C62A-0A49-403A-B67E-48E3CCC26760'
 
-// GET /v1/exchangerate/BTC?apikey=73034021-THIS-IS-SAMPLE-KEY
-
-// window.addEventListener("load", function() {
-//     const url = 'https://rest.coinapi.io/v1/assets';
-//     fetch(url, {
-//         method: "GET",
-//         headers: {
-//         "X-CoinAPI-Key": API_KEY,
-//         "Accept": "application/json",
-//         "Accept-Encoding": "deflate", "gzip"
-//         }
-//     })
-//     .then(resp => resp.json())
-//     .then(function(data) {
-//         console.log(data);
-        
-
-
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     })
-// });
-
+// Method to create the list of data that we retrieve from the API. The data points we need are the id, name, and price
 const create_list = (asset_id, asset_name, price_usd) => {
-    const html = `<tr id="coin_data"><th scope="row" id="coin_data">${asset_id}</th><td id="name">${asset_name}</td><td id="price_usd">$${price_usd}</td></tr>`
+    const html = `<tr class="data_list" id="coin_data"><th scope="row" id="asset_id" onclick="get_exchange_rate(this.innerText)">${asset_id}</th><td id="name">${asset_name}</td><td id="price_usd">$${price_usd}</td></tr>`
     document.getElementById('data_table').insertAdjacentHTML('beforeend', html)
 }
 
+// method to load the table whenever the webpage loads
 window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
     get_data(event)
 });
 
+// Method to call the API to grab the data. We need only specific data points so we'll set those datapoints as variables
 const get_data = async () => {
     let response = await axios.get(`https://rest.coinapi.io/v1/assets/?apikey=${API_KEY}`)
     console.log(response);
 
-    const all = response.data;
+    var all = response.data;
 
     // Declare variables that will hold the data for our table
     let asset_id = ''
@@ -61,8 +41,17 @@ const get_data = async () => {
 }   
 }
 
+get_exchange_rate = async (id) => {
+        let txt = id;
+        // console.log(txt)
+    let response = await axios.get(`https://rest.coinapi.io/v1/assets/${txt}?apikey=${API_KEY}`)
+    // console.log(response)
+    // console.log(response.data)
+    // console.log(response.data[0])
+    alert(JSON.stringify(response.data[0], null, 4))
+}
 
-
+// Method to see just 1 or multiple data points based on search criteria, eg. BTC or ETH
 const get_one_data = async (search_name) => {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
@@ -86,41 +75,18 @@ const get_one_data = async (search_name) => {
     
 }
 
-// Filter/search function for specific asset ID
+// Filter/search function for specific asset ID which will run when the user hits the submit button
 form.addEventListener('submit', ( event ) => {
     event.preventDefault();
     let search_name = document.getElementById('coin_id');
     get_one_data(search_name)
 })
 
-// Refresh the table back to default of all assets
-form.addEventListener('reset', logReset);
-
-
-
-
-// asset_id: "BTC",
-// name: "Bitcoin",
-// type_is_crypto: 1,
-// data_start: "2010-07-17",
-// data_end: "2021-07-24",
-// data_quote_start: "2014-02-24T17:43:05.0000000Z",
-// data_quote_end: "2021-07-24T22:11:32.3816757Z",
-// data_orderbook_start: "2014-02-24T17:43:05.0000000Z",
-// data_orderbook_end: "2020-08-05T14:38:38.3413202Z",
-// data_trade_start: "2010-07-17T23:09:17.0000000Z",
-// data_trade_end: "2021-07-24T22:11:19.6100000Z",
-// data_symbols_count: 59410,
-// volume_1hrs_usd: 1361682626266.55,
-// volume_1day_usd: 65506462020325.15,
-// volume_1mth_usd: 2553673061115921.5,
-// price_usd: 33970.33322796611,
-// id_icon: "4caf2b16-a017-4e26-a348-2cea69c34cba"
-
+// DOM manipulation create the table based on these criteria and headers
 const create_table = () => {
     const htmltest = 
     
-        `<thead><tr class="clickable">
+        `<thead><tr class="clickable row100" id="asset_id">
         <th scope="col" id="coin_name">ID</th>
         <th scope="col">Name</th>
         <th scope="col">Price in USD</th>
